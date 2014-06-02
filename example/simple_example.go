f611f8cec4359fd5e34e151c7d855aeadc0b8f0d
Eric Walker <eric.ewalker@gmail.com>
2014-06-02 20:36:51 +0200

    Copy remaining comments and credit original author

--- example/simple_example.go
+++ example/simple_example.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/ewalk153/fheap"
+	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(22)
+	time.Now()
+	// rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
 	h := fheap.FibHeap{}
-	h.Enqueue(5, 3)
-	h.Enqueue(2, 5)
-	h.Enqueue(1, 9)
-	for h.Len() > 0 {
-		x := h.DequeueMin()
-		fmt.Println("Poppping", x)
+	for i := 0; i < 20; i++ {
+		h.Enqueue(i, rand.Float64())
+	}
+	for x := h.DequeueMin(); x != nil; x = h.DequeueMin() {
+		fmt.Println("Min", x.Element)
 	}
 }
--- fheap.go
+++ fheap.go
@@ -1,3 +1,57 @@
+/***********************************************************************
+ * File: fheap.go (Original was FibonacciHeap.java)
+ * Author: Keith Schwarz (htiek@cs.stanford.edu)
+ * Adapted for Go by Eric Walker
+ *
+ * An implementation of a priority queue backed by a Fibonacci heap,
+ * as described by Fredman and Tarjan.  Fibonacci heaps are interesting
+ * theoretically because they have asymptotically good runtime guarantees
+ * for many operations.  In particular, insert, peek, and decrease-key all
+ * run in amortized O(1) time.  dequeueMin and delete each run in amortized
+ * O(lg n) time.  This allows algorithms that rely heavily on decrease-key
+ * to gain significant performance boosts.  For example, Dijkstra's algorithm
+ * for single-source shortest paths can be shown to run in O(m + n lg n) using
+ * a Fibonacci heap, compared to O(m lg n) using a standard binary or binomial
+ * heap.
+ *
+ * Internally, a Fibonacci heap is represented as a circular, doubly-linked
+ * list of trees obeying the min-heap property.  Each node stores pointers
+ * to its parent (if any) and some arbitrary child.  Additionally, every
+ * node stores its degree (the number of children it has) and whether it
+ * is a "marked" node.  Finally, each Fibonacci heap stores a pointer to
+ * the tree with the minimum value.
+ *
+ * To insert a node into a Fibonacci heap, a singleton tree is created and
+ * merged into the rest of the trees.  The merge operation works by simply
+ * splicing together the doubly-linked lists of the two trees, then updating
+ * the min pointer to be the smaller of the minima of the two heaps.  Peeking
+ * at the smallest element can therefore be accomplished by just looking at
+ * the min element.  All of these operations complete in O(1) time.
+ *
+ * The tricky operations are dequeueMin and decreaseKey.  dequeueMin works
+ * by removing the root of the tree containing the smallest element, then
+ * merging its children with the topmost roots.  Then, the roots are scanned
+ * and merged so that there is only one tree of each degree in the root list.
+ * This works by maintaining a dynamic array of trees, each initially null,
+ * pointing to the roots of trees of each dimension.  The list is then scanned
+ * and this array is populated.  Whenever a conflict is discovered, the
+ * appropriate trees are merged together until no more conflicts exist.  The
+ * resulting trees are then put into the root list.  A clever analysis using
+ * the potential method can be used to show that the amortized cost of this
+ * operation is O(lg n), see "Introduction to Algorithms, Second Edition" by
+ * Cormen, Rivest, Leiserson, and Stein for more details.
+ *
+ * The other hard operation is decreaseKey, which works as follows.  First, we
+ * update the key of the node to be the new value.  If this leaves the node
+ * smaller than its parent, we're done.  Otherwise, we cut the node from its
+ * parent, add it as a root, and then mark its parent.  If the parent was
+ * already marked, we cut that node as well, recursively mark its parent,
+ * and continue this process.  This can be shown to run in O(1) amortized time
+ * using yet another clever potential function.  Finally, given this function,
+ * we can implement delete by decreasing a key to -\infty, then calling
+ * dequeueMin to extract it.
+ */
+
 package fheap
 
 import (
@@ -38,7 +92,7 @@ func (f *FibHeap) Enqueue(element interface{}, priority float64) *Entry {
 	f.checkPriority(priority)
 	result := newEntry(element, priority)
 	f.min = mergeLists(f.min, result)
-	f.size += 1
+	f.size++
 	return result
 }
 
@@ -56,6 +110,22 @@ func (f *FibHeap) checkPriority(priority float64) {
 	//   throw new IllegalArgumentException(priority + " is invalid.");
 }
 
+/**
+ * Utility function which, given two pointers into disjoint circularly-
+ * linked lists, merges the two lists together into one circularly-linked
+ * list in O(1) time.  Because the lists may be empty, the return value
+ * is the only pointer that's guaranteed to be to an element of the
+ * resulting list.
+ *
+ * This function assumes that one and two are the minimum elements of the
+ * lists they are in, and returns a pointer to whichever is smaller.  If
+ * this condition does not hold, the return value is some arbitrary pointer
+ * into the doubly-linked list.
+ *
+ * @param one A pointer into one of the two linked lists.
+ * @param two A pointer into the other of the two linked lists.
+ * @return A pointer to the smallest element of the resulting list.
+ */
 func mergeLists(one, two *Entry) *Entry {
 	if one == nil && two == nil {
 		return nil
@@ -64,6 +134,7 @@ func mergeLists(one, two *Entry) *Entry {
 	} else if one == nil && two != nil {
 		return two
 	}
+
 	// Both non-null; actually do the splice.
 	/* This is actually not as easy as it seems.  The idea is that we'll
 	 * have two lists that look like this:
@@ -135,7 +206,8 @@ func (f *FibHeap) DequeueMin() *Entry {
 	if f.size == 0 {
 		return nil
 	}
-	f.size -= 1
+	f.size--
+
 	minElem := f.min
 
 	/* Now, we need to get rid of this element from the list of roots.  There
@@ -299,6 +371,14 @@ func (f *FibHeap) DecreaseKey(e *Entry, newPriority float64) error {
 
 var ErrorExceedsPriority = errors.New("Error new priority exceeds old")
 
+/**
+ * Deletes this Entry from the Fibonacci heap that contains it.
+ *
+ * It is assumed that the entry belongs in this heap.  For efficiency
+ * reasons, this is not checked at runtime.
+ *
+ * @param entry The entry to delete.
+ */
 func (f *FibHeap) Delete(e *Entry) {
 	/* Use decreaseKey to drop the entry's key to -infinity.  This will
 	 * guarantee that the node is cut and set to the global minimum.
@@ -376,7 +456,7 @@ func (f *FibHeap) cutNode(e *Entry) {
 	}
 
 	/* Decrease the degree of the parent, since it just lost a child. */
-	e.parent.degree -= 1
+	e.parent.degree--
 
 	/* Splice this tree into the root list by converting it to a singleton
 	 * and invoking the merge subroutine.
